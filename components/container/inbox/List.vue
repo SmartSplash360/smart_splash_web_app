9bfc67d5d101501165038f2960d552fc74958e2f
jordy_tshibss <jordytshibss@gmail.com>
2023-06-26 17:53:36 +0200

    features : inbox page styling

--- components/container/inbox/List.vue
+++ components/container/inbox/List.vue
@@ -5,7 +5,9 @@
     <div
       class="flex max-h-[130px] w-full flex-col gap-5 border-r sm:max-h-full sm:w-[30%] sm:pt-12"
     >
-      <RegularCustomerChatPreview :chats="chats"></RegularCustomerChatPreview>
+      <RegularCustomerChatPreview
+        :chats="inboxService"
+      ></RegularCustomerChatPreview>
     </div>
     <div class="sm:w-[70%]">
       <RegularInboxChat></RegularInboxChat>
@@ -14,66 +16,5 @@
 </template>
 
 <script setup>
-const chats = ref([
-  {
-    username: "A.J Beson",
-    subject: "Inbox subject",
-    message:
-      "Lorem ipsum dolor sit amet consectetur Adipiscing sit vitae. Adipiscing sit vitae Adipiscing sit vitae fringilla  rhoncus..",
-  },
-  {
-    username: "Mike Beson",
-    subject: "Inbox subject",
-    message:
-      "Lorem ipsum dolor sit amet consectetur Adipiscing Adipiscing sit vitae Adipiscing sit vitae sit vitae. Adipiscing sit vitae Adipiscing sit vitae fringilla  rhoncus..",
-  },
-  {
-    username: "Sarah Johns",
-    subject: "Inbox subject",
-    message:
-      "Lorem ipsum dolor sit amet consectetur Adipiscing sit vitae. Adipiscing sit vitae Adipiscing sit vitae fringilla  rhoncus..",
-  },
-  {
-    username: "A.J Beson",
-    subject: "Inbox subject",
-    message:
-      "Lorem ipsum dolor sit amet consectetur Adipiscing sit vitae. Adipiscing sit vitae Adipiscing sit vitae fringilla  rhoncus..",
-  },
-  {
-    username: "Mike Beson",
-    subject: "Inbox subject",
-    message:
-      "Lorem ipsum dolor sit amet consectetur Adipiscing sit vitae. Adipiscing sit vitae Adipiscing sit vitae fringilla  rhoncus..",
-  },
-  {
-    username: "A.J Beson",
-    subject: "Inbox subject",
-    message:
-      "Lorem ipsum dolor sit amet consectetur Adipiscing sit vitae. Adipiscing sit vitae Adipiscing sit vitae fringilla  rhoncus..",
-  },
-  {
-    username: "Mike Beson",
-    subject: "Inbox subject",
-    message:
-      "Lorem ipsum dolor sit amet consectetur Adipiscing sit vitae. Adipiscing sit vitae Adipiscing sit vitae fringilla  rhoncus..",
-  },
-  {
-    username: "Sarah Johns",
-    subject: "Inbox subject",
-    message:
-      "Lorem ipsum dolor sit amet consectetur Adipiscing sit vitae. Adipiscing sit vitae Adipiscing sit vitae fringilla  rhoncus..",
-  },
-  {
-    username: "A.J Beson",
-    subject: "Inbox subject",
-    message:
-      "Lorem ipsum dolor sit amet consectetur Adipiscing sit vitae. Adipiscing sit vitae Adipiscing sit vitae fringilla  rhoncus..",
-  },
-  {
-    username: "Mike Beson",
-    subject: "Inbox subject",
-    message:
-      "Lorem ipsum dolor sit amet consectetur Adipiscing sit vitae. Adipiscing sit vitae Adipiscing sit vitae fringilla  rhoncus..",
-  },
-]);
+import { inboxService } from "@/services/InboxServices";
 </script>
--- components/regular/customer/ChatPreview.vue
+++ components/regular/customer/ChatPreview.vue
@@ -3,44 +3,28 @@
   <ul
     class="flex flex-row overflow-x-auto overflow-y-hidden sm:flex-col sm:overflow-auto"
   >
-    <li v-for="chat in chats" class="min-h-[130px] border-b">
+    <li v-for="chat in chats" class="min-h-[180px] border-b">
       <div
-        class="flex cursor-pointer items-center gap-1 rounded-lg bg-[#D9D9D9] px-2 py-3 hover:bg-[#D9D9D9] sm:h-full sm:bg-white sm:p-0"
+        class="flex cursor-pointer items-center rounded-lg bg-[#D9D9D9] px-2 py-3 hover:bg-[#D9D9D9] sm:h-full sm:bg-white sm:p-0"
       >
-        <div class="hidden pl-5 pr-2 sm:block">
-          <Avatar
-            :image="userProfile"
-            class="mr-2"
-            size="xlarge"
-            shape="circle"
-          />
-        </div>
-        <div class="flex sm:hidden">
-          <Avatar
-            :image="userProfile"
-            class="mr-2"
-            size="xmedium"
-            shape="circle"
-          />
-        </div>
-        <div class="flex w-full flex-col gap-2">
+        <img
+          :src="chat.image"
+          class="ml-5 h-[65px] w-[65px] rounded-full object-cover"
+        />
+        <div class="flex flex-col gap-2 px-5">
           <div class="flex w-full items-center justify-between">
-            <h4 class="text-xs font-[500] leading-[15px] sm:text-[18px]">
-              {{ chat.username }}
+            <h4 class="text-lg font-[500] text-gray-700">
+              {{ chat.name }}
             </h4>
-            <span class="self-start pr-5 text-[12px] font-[400] leading-[16px]"
-              >9:44 PM</span
-            >
+            <span class="text-xs text-gray-700"> {{ chat.time }} AM</span>
           </div>
 
-          <p class="hidden flex-col gap-2 sm:flex">
-            <span class="hidden text-[15px] font-[600] leading-[20px] sm:flex">
-              {{ chat.subject }}
-            </span>
-            <span class="text-[13px] font-[400] leading-[16px]">
-              {{ chat.message }}</span
-            >
-          </p>
+          <span class="text-sm font-bold">
+            {{ chat.subject }}
+          </span>
+          <span class="max-h-[60px] overflow-hidden text-ellipsis text-sm">
+            {{ chat.message }}
+          </span>
         </div>
       </div>
     </li>
@@ -48,8 +32,6 @@
 </template>
 
 <script setup>
-import userProfile from "@/assets/images/profile_user.jpg";
-
 const props = defineProps({
   chats: Array,
 });
