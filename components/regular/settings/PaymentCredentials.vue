d7fe95b5413209e5e67966a45ccf813e9f9ad8d2
Jordy Bukonda Tshibangu <69379641+JordyBukondaTshibangu@users.noreply.github.com>
2023-07-31 11:36:43 +0200

    features : create tenants

--- components/regular/settings/PaymentCredentials.vue
+++ components/regular/settings/PaymentCredentials.vue
@@ -16,7 +16,7 @@
           severity="secondary"
           outlined
         />
-        <Button label="Save changes" class="!bg-[#0291BF] hover:shadow-lg" />
+        <Button label="Save changes" class="!bg-[#0291BF] hover:shadow-lg text-white" @click="handleSaveChange"/>
       </div>
     </div>
     <div class="flex flex-col gap-10 border-b dark:border-b-gray-600  py-10 sm:flex-row xl:gap-64">
@@ -27,22 +27,23 @@
         </span>
       </div>
       <div class="flex h-full w-full flex-col gap-3">
-        <BasePaymentMethodCard
-          :icon="VisaIcon"
-          :alt="'visa-icon'"
-          :name="'Visa'"
-          :expiringDate="'06/2024'"
-          :method="method"
-        ></BasePaymentMethodCard>
-        <BasePaymentMethodCard
-          :icon="MasterCardIcon"
-          :alt="'master-card-icon'"
-          :name="'MasterCard'"
-          :expiringDate="'06/2024'"
-          :method="method"
-        ></BasePaymentMethodCard>
-
-        <div class="mt-5 flex items-center gap-3 px-6">
+      <div v-for="payment in paymentMethods" class="flex h-full w-full flex-col gap-3">
+          <BasePaymentMethodCard
+            :payment="payment"
+            :icon="payment.icon"
+            :alt="payment.alt"
+            :name="payment.name"
+            :method="method"
+            :handleEdit="editCardMethod"
+          ></BasePaymentMethodCard>
+        </div>
+        <ModalsPaymentEditCreditCard 
+          v-if="showEditPaymentModal"
+          :payment="card"
+          :toggleEditCreditCardModal="closeModal">
+        </ModalsPaymentEditCreditCard>
+      
+        <div class="mt-5 flex items-center gap-3 px-6 cursor-pointer" @click="toggleEditCreditCardModal">
           <span class="span__element">+</span>
           <span class="span__element">Add new payment method</span>
         </div>
@@ -77,5 +78,61 @@ import MasterCardIcon from "@/assets/images/logos_mastercard.svg";
 import QuickBookIcon from "@/assets/images/quickbooks-icon.svg";
 
 const method = ref("");
-const checked = ref(true);
+const checked = ref(false);
+const showEditPaymentModal= ref(false)
+const paymentMethods = ref([
+  {
+    icon : VisaIcon,
+    alt: 'visa-icon',        
+    name :'Visa',
+    method : method.value,
+    paymentDetails  : {
+        cardNumber : '123431234',
+        expiringDate : '06/2024',
+        cvv : '2343',
+        firstname : 'Jack',
+        lastname : 'Ma'
+    }
+  },
+  {
+    icon : MasterCardIcon,
+    alt: 'master-card-icon',        
+    name :'MasterCard',
+    method:method.value,
+    paymentDetails  : {
+        cardNumber : '32874387',
+        expiringDate : '12/2026',
+        cvv : '7686',
+        firstname : 'Elon',
+        lastname : 'Musk'
+    }
+  }
+])
+const card = ref()
+const handleSelectMethod = (value) => {
+  console.log(value)
+}
+const toggleEditCreditCardModal = () => { 
+  card.value = null
+  showEditPaymentModal.value = !showEditPaymentModal.value
+}
+const editCardMethod = (item) => {
+  card.value = item
+  showEditPaymentModal.value = !showEditPaymentModal.value
+}
+const closeModal = ({ success, error }) => {
+  showEditPaymentModal.value = false
+  if (success) {
+    toast.add({ severity: 'success', summary: 'Alerts', detail: success, life: 3000 });
+  }
+
+  if (error) {
+    toast.add({ severity: 'error', summary: 'Alerts', detail: `An error has occurred: ${error}`, life: 3000 });
+  }
+};
+
+const handleSaveChange = () => {
+  console.log(method.value)
+}
+
 </script>
---
+++ stores/tenant.ts
@@ -0,0 +1,54 @@
+import {defineStore} from "pinia";
+import axios from "axios";
+
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common['Accept'] = 'application/json';
+// axios.defaults.withCredentials = true;
+
+export const useTenantStore = defineStore("tenant", {
+    persist: {
+        storage: persistedState.localStorage,
+    },
+    state: () => ({
+        loggedIn: false,
+        currentTenant: null,
+        jwt: "",
+        tenants: [],
+    }),
+    getters: {
+        getTenants(state) {
+            return state.tenants;
+        },
+        getCurrentTenant(state) {
+            return state.currentTenant;
+        },
+        getJwt(state) {
+            return state.jwt;
+        },
+        getLoggedIn(state) {
+            return state.loggedIn;
+        },
+    },
+    actions: {
+        async register(userPayload: {}) {
+            try {
+                const res = await axios.post("http://localhost:8000/api/v1/tenant", userPayload);
+                this.currentTenant = res.data;
+            } catch (error) {
+                alert(error)
+                console.log(error)
+            }
+        },
+        async fetchUsers() {
+            try {
+                const data = await axios.get(
+                    "https://jsonplaceholder.typicode.com/tenant"
+                );
+                this.tenants = data.data;
+            } catch (error) {
+                alert(error);
+                console.log(error);
+            }
+        },
+    },
+});
